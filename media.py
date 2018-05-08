8ff96482882bbf60491c000c6c916af630defa9f
Ben Conway <benrconway84@gmail.com>
2018-05-08 16:57:04 +0100

    Updated project, including gitignore

--- media.py
+++ media.py
@@ -1,1 +1,8 @@
-# The information of the class for Movie will be here
+# As per the requirements of our Project, this file will contain the class for
+# our movie listing website
+
+class Movie():
+    def __init__(self, title, poster_url, trailer_url):
+        self.title = title
+        self.poster_image_url = poster_url
+        self.trailer_youtube_url = trailer_url
