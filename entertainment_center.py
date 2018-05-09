5c2db44933d2bc8e1482e4b7465dfed689c7b94d
Ben Conway <benrconway84@gmail.com>
2018-05-09 08:59:45 +0100

    Revised from notes attached to submission

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,30 +6,23 @@ import fresh_tomatoes
 # All instances need a title, url for poster art and a url for a
 # youtube trailer
 predator = media.Movie("Predator",
-                       "https://cdn.shopify.com/s/files/1/1416/8662/product"
-                       "s/predator_1987_turkey_original_film_art_800x.jpg?"
-                       "v=1522356369",
+                       "https://cdn.shopify.com/s/files/1/1416/8662/products/predator_1987_turkey_original_film_art_800x.jpg?v=1522356369",  # NOQA
                        "https://www.youtube.com/watch?v=K9AT3tQGbIk")
 
 street_fighter = media.Movie("Street Fighter 2: The Animated Movie",
-                             "https://vignette.wikia.nocookie.net/"
-                             "streetfighter/images/7/7a/SFII_Animated_Movie_"
-                             "poster.jpg/revision/latest?cb=20081125205612",
+                             "https://vignette.wikia.nocookie.net/streetfighter/images/7/7a/SFII_Animated_Movie_poster.jpg/revision/latest?cb=20081125205612",  # NOQA
                              "https://www.youtube.com/watch?v=_xlb65hp4lo")
 
 shawshank = media.Movie("The Shawshank Redemption",
-                        "https://uk.movieposter.com/posters/archive/main/42/"
-                        "MPW-21321",
+                        "https://uk.movieposter.com/posters/archive/main/42/MPW-21321",  # NOQA
                         "https://www.youtube.com/watch?v=6hB3S9bIaco")
 
 thor_ragnarok = media.Movie("Thor Ragnarok",
-                            "http://www.impawards.com/2017/posters/thor_"
-                            "ragnarok.jpg",
+                            "http://www.impawards.com/2017/posters/thor_ragnarok.jpg",  # NOQA
                             "https://www.youtube.com/watch?v=ue80QwXMRHg")
 
 mononoke = media.Movie("Princess Mononoke",
-                       "https://i.pinimg.com/originals/a4/7f/e8/a47fe8c7"
-                       "0b2e5885463f07e1cd7a7104.jpg",
+                       "https://i.pinimg.com/originals/a4/7f/e8/a47fe8c70b2e5885463f07e1cd7a7104.jpg",  # NOQA
                        "https://www.youtube.com/watch?v=4OiMOHRDs14")
 
 ponyo = media.Movie("Ponyo on the Cliff by the Sea",
